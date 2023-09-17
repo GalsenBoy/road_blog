5e26e45f4b657f0cfc435f2e7dc82e44f6b0a2be
bilaly <billycsk@gmail.com>
2023-09-17 14:13:23 +0200

    Ajout de useFom

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import UserList from "../composable/UserList";
 import { IUser } from "../interfaces/IUser";
-import DeleteUser from "../composable/DeleteUser";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
 
 export default function UserComponent() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  console.log(watch("example"));
   const [users, setUsers] = useState<IUser[]>([
     {
       id: 1,
@@ -28,15 +42,28 @@ export default function UserComponent() {
   ]);
 
   const handleDelete = (userId: number) => {
-    const updatedUsers = DeleteUser(users, userId);
+    const updatedUsers = users.filter((user) => user.id !== userId);
     setUsers(updatedUsers);
   };
 
   return (
-    <div>
-      {users.map((user) => (
-        <UserList user={user} handleDelete={handleDelete} />
-      ))}
-    </div>
+    <>
+      <div>
+        {users.map((user) => (
+          <UserList user={user} handleDelete={handleDelete} />
+        ))}
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register("example")} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
+    </>
   );
 }
--- src/composable/DeleteUser.tsx
+++
@@ -1,6 +0,0 @@
-import { IUser } from "../interfaces/IUser";
-
-export default function DeleteUser(users: IUser[], userId: number): IUser[] {
-  const updatedUsers = users.filter((user) => user.id !== userId);
-  return updatedUsers;
-}
