7344bd4277dfc5da288c4a981da9a00cb4bcae8a
GalsenBoy <vipandco3@gmail.com>
2023-10-12 15:16:02 +0200

    Réception des données avec axios + config cors

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,9 +1,31 @@
+import { useState,useEffect } from "react";
 import "../css/card.scss";
+import IPost from "../interfaces/IPost";
+import axios from "axios";
+import { API_REMOTE } from "../routes";
 
 export default function Card() {
+  const [posts, setPosts] = useState<IPost>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const allPost = async () => {
+    try {
+      const response = await axios.get(API_REMOTE);
+      setPosts(response.data);
+      console.log(response.data)
+      setIsLoading(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    allPost()
+  },[])
+
   return (
     <section>
-      <div id="card-container">
+      {isLoading? ( <div id="card-container">
         <div id="photo-post">
           <img src="/nature.jpg" alt="" />
         </div>
@@ -17,8 +39,13 @@ export default function Card() {
           </div>
         </div>
         <h1>Le titre du blog</h1>
-        <p id="contenu-blog">Le contenu du blog fefffffff effffffffffff efffffffff effffffff</p>
-      </div>
+        <p id="contenu-blog">
+          Le contenu du blog fefffffff effffffffffff efffffffff effffffff
+        </p>
+      </div>):(
+        <p>C'est entrain de charger bg attend un peu</p>
+      )}
+     
     </section>
   );
 }
