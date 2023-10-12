7b72380082b2aca3bba70552b95865ce4468ec39
GalsenBoy <vipandco3@gmail.com>
2023-10-12 11:11:03 +0200

    Ajout de scss + config

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,1 +1,24 @@
-export default function Card(){}
+import "../css/card.scss";
+
+export default function Card() {
+  return (
+    <section>
+      <div id="card-container">
+        <div id="photo-post">
+          <img src="/nature.jpg" alt="" />
+        </div>
+        <div id="personne">
+          <div id="photo-profil">
+            <img src="/woman.jpg" alt="" />
+          </div>
+          <div id="identite">
+            <p>Fatima Zeynab</p>
+            <p>Publié le 12/10/2023</p>
+          </div>
+        </div>
+        <h1>Le titre du blog</h1>
+        <p>Le contenu du blog</p>
+      </div>
+    </section>
+  );
+}
