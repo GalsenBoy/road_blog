7fa5ce445308d11cfdede58d36710246bd2b9d69
GalsenBoy <vipandco3@gmail.com>
2023-11-13 12:13:22 +0100

    Ajout de mixins + squelette formulaire de création de post

--- src/components/BlogForm.tsx
+++ src/components/BlogForm.tsx
@@ -1,17 +1,19 @@
+import Button from "../composable/Button";
 import Input from "../composable/Input";
-import "../css/blog_form.scss"
+import "../css/blog_form.scss";
 
 export default function BlogForm() {
   return (
     <section>
       <form action="">
         <div id="form-container">
-          <div>
+          <div className="input-group">
             <Input placeholder="Titre du blog" type="text" />
           </div>
-          <div>
-            <Input placeholder="Contenu du blog" type="text" />
+          <div className="input-group">
+            <textarea placeholder="Contenu du blog" rows={7}></textarea>
           </div>
+          <Button content="Publier"/>
         </div>
       </form>
     </section>
