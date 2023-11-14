62bea5a3f9373dad87f1b48a23c41c8aef872ddd
GalsenBoy <vipandco3@gmail.com>
2023-11-14 09:52:18 +0100

    migration fastify + création de notre première route + installation de dépendance nécessaire

--- Server/index.js
+++ Server/index.js
@@ -1,27 +1,48 @@
 import fastify from "fastify";
 import mysql from "mysql2";
+import cors from '@fastify/cors'
+
 
 const app = fastify();
+await app.register(cors, { 
+  // put your options here
+})
+
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'my_blog'
-  });
+  host: "localhost",
+  user: "root",
+  database: "my_blog",
+});
 
-app.get("/", (req, res) => {
+app.register(import("@fastify/routes"));
+
+app.get("/", async (req, res) => {
   try {
-    const result =  db.query("SELECT * FROM post");
-    res.send(result);
-  } catch (err) {
-    res.send(err);
+    const queryResults = await getPosts(); // Attend que la fonction getPosts soit terminée
+    res.send(queryResults);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erreur interne du serveur");
   }
 });
+
+const getPosts = () => {
+  return new Promise((resolve, reject) => {
+    db.query("SELECT * FROM `post`", function (err, results) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  });
+};
 
 const start = async () => {
   try {
     await app.listen({ port: 5000 });
-    console.log(`Server running on ${app.server.address().port}`);
+    console.log(`Serveur démarré sur le port ${app.server.address().port}`);
   } catch (error) {
     console.error(error);
     /* eslint-disable no-undef */
