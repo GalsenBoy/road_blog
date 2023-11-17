0c3ea362373eaad98240988cf9101e4ed7596592
GalsenBoy <vipandco3@gmail.com>
2023-11-17 11:42:41 +0100

    Surcharge de mon controller

--- Server/src/post/post.controller.ts
+++ Server/src/post/post.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get,Post,Delete } from '@nestjs/common';
 import { PostService } from './post.service';
+import { IPost } from 'interfaces/IPost';
 
-@Controller('post')
+@Controller('posts')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+  @Get()
+  findAll(){
+    return this.postService.findAll();
+  }
+
+  @Post()
+  create(post:IPost){
+    return this.postService.create(post);
+  }
+
+  @Get()
+  findOne(id:number){
+    return this.postService.findOne(id)
+  }
+
+  @Delete()
+  delete(id:number){
+    return this.postService.delete(id)
+  }
 }
