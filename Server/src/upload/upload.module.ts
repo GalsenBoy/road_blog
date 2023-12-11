462543b55d0bbb15784f3438e813af1991aa58e9
GalsenBoy <vipandco3@gmail.com>
2023-12-11 15:32:11 +0100

    création du service upload

--- Server/src/upload/upload.module.ts
+++ Server/src/upload/upload.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { UploadController } from './upload.controller';
 import { Upload } from './upload.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UploadService } from './upload.service';
 
 @Module({
   controllers: [UploadController],
   imports: [TypeOrmModule.forFeature([Upload])],
+  providers: [UploadService],
 })
 export class UploadModule { }
