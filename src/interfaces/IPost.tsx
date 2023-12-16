a40c43073d7e98d9ecc2ad35f70c923364b66a90
bilaly <billycsk@gmail.com>
2023-12-16 10:58:19 +0100

    Suppresion des fichiers inutilisé

--- src/interfaces/IPost.tsx
+++ src/interfaces/IPost.tsx
@@ -1,6 +1,6 @@
 export default interface IPost {
-    id:number,
-    title :string,
-    content:string,
-    createAt?:Date,
+  id: string;
+  title: string;
+  description: string;
+  createAt: Date;
 }
